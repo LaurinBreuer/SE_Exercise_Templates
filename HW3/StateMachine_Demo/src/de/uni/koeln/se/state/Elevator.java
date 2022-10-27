21c4ee9dd0c884994bc0e30de590fa5f6d537bbb
lbreue13 <lbreue13@smail.uni-koeln.de>
2022-10-27 11:51:03 +0200

    HW 3 Task 3 solved

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
@@ -11,14 +11,57 @@ public class Elevator{
 	public Elevator (int dest_floor)
 	{
 		this.dest_Floor=dest_floor;
-	System.out.println("*** Destination floor is: "+ dest_Floor);
-	arrive_atFloor();
-
-
+		System.out.println("*** Destination floor is: "+ dest_Floor);
+		arrive_atFloor();
 	}
 
+	// The current floor equals the destination floor
 	private void arrive_atFloor() {
-		
+		// Set the state
+		State.Current_State = State.Idle;
+		System.out.println("Current state: " + State.Current_State + " | Current floor: "+ current_Floor );
+
+		if (current_Floor < dest_Floor){ // The destination floor is above the current floor
+			go_up();
+		}
+
+		else if (current_Floor > dest_Floor){ // The destination floor is below the current floor
+			go_down();
+		}
+
+		else if (current_Floor == dest_Floor){ // Arrived at the destination floor
+			System.out.println("Exit. You have arrived at your destination floor");
+		}
+	}
+
+	private void go_up(){
+		State.Current_State = State.Moving_up;
+		current_Floor++;
+		System.out.println("Current state: " + State.Current_State + " | Current floor: "+ current_Floor );
+
+		if (current_Floor < dest_Floor){
+			go_up();
+
+		}
+
+		else if (current_Floor == dest_Floor){
+			arrive_atFloor();
+		}
+	}
+
+	private void go_down(){
+		State.Current_State = State.Moving_down;
+		current_Floor--;
+		System.out.println("Current state: " + State.Current_State + " | Current floor: "+ current_Floor );
+
+		if (current_Floor > dest_Floor){
+
+			go_down();
+		}
+
+		else if (current_Floor == dest_Floor){
+			arrive_atFloor();
+		}
 	}
 
 	
